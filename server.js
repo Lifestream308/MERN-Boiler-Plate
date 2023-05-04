f3c653e5dc7b0f0611415d6620625b4d5348018a
Christian Arroyo <ChristianArroyo308@yahoo.com>
2023-05-03 21:01:37 -0700

    first commit. Basic working MongoDB Express Mongoose app

---
+++ server.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+
+const express = require("express")
+const app = express()
+const mongoose = require('mongoose')
+
+
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database'))
+
+app.use(express.json())
+
+app.use('/api/goals', require('./routes/goalRoutes.js'));
+
+app.get("/", (req, res) => {
+    res.send('Express App is working!')
+})
+
+
+app.listen(3000)
